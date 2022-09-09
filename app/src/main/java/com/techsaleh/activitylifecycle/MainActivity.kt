a8247f4c35b6a07714b5376d487654da8ef6d4ed
sadan49 <whiteheat2019@gmail.com>
2022-09-09 11:23:20 -0400

    Change Title Bar Name

--- app/src/main/java/com/techsaleh/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/techsaleh/activitylifecycle/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.scrollview_example)
+
+        title = "The Scroll View"
     }
 
     override fun onStart() {
