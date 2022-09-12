208f95ede337e3f0b095ef273bea21670a930d75
sadan49 <whiteheat2019@gmail.com>
2022-09-12 06:02:26 -0400

    Saving Login Preferences using SharedPreferences.

--- foodrunner/src/main/java/com/intern/foodrunner/LoginActivity.kt
+++ foodrunner/src/main/java/com/intern/foodrunner/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.intern.foodrunner
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,6 +12,7 @@ import android.widget.TextView
 class LoginActivity : AppCompatActivity() {
 
     /*Declaring the different variables used for this activity*/
+    private lateinit var sharedPreferences: SharedPreferences
     private lateinit var registerYourself: TextView
     private lateinit var login: Button
     private lateinit var etMobileNumber: EditText
@@ -22,6 +25,19 @@ class LoginActivity : AppCompatActivity() {
 
         /*Linking the view*/
         setContentView(R.layout.activity_login)
+
+        /*Implementing SharedPrefrences*/
+        sharedPreferences = getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
+
+        val isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false)
+
+        if (isLoggedIn) {
+            val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        title = "Log In"
 
         /*Initialising the views with the ones defined in the XML*/
         etMobileNumber = findViewById(R.id.etMobileNumber)
@@ -38,6 +54,10 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
         }
         login.setOnClickListener {
+
+            /*Declaring Value*/
+            var s = "Dashboard"
+            savePreferences(s)
 
             /*Declaring the intent which sets up the route for the navigation of the activity*/
             val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
@@ -61,5 +81,15 @@ class LoginActivity : AppCompatActivity() {
             /*Starting the new activity by sending the intent in the startActivity method*/
             startActivity(intent)
         }
+    }
+    /* When user clicks back button this activity need to be finished. */
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+
+    private fun savePreferences(title: String) {
+        sharedPreferences.edit().putBoolean("isLoggedIn", true).apply()
+        sharedPreferences.edit().putString("title", title).apply()
     }
 }
