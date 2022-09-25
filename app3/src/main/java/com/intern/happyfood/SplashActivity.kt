8552e30942ac92d8fa8b2b847c7e0ee35fb980cd
sadan49 <whiteheat2019@gmail.com>
2022-09-25 04:54:56 -0400

    Happy Food Project

---
+++ app3/src/main/java/com/intern/happyfood/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.intern.happyfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
