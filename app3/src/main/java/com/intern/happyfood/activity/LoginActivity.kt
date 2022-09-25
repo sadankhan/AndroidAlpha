0e69fb933644ece20749d588e49342fd89539b64
sadan49 <whiteheat2019@gmail.com>
2022-09-25 05:57:32 -0400

    Add Splash Activity.

--- app3/src/main/java/com/intern/happyfood/activity/LoginActivity.kt
+++ app3/src/main/java/com/intern/happyfood/activity/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.intern.happyfood
+package com.intern.happyfood.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.intern.happyfood.R
 
-class SplashActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_login)
     }
 }
