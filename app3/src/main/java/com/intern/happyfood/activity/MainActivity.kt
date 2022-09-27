7d1b46dc2175bf005fcdd2917d1ae47a0ffd1135
sadan49 <whiteheat2019@gmail.com>
2022-09-27 09:37:11 -0400

    Add Navigation Drawer.

--- app3/src/main/java/com/intern/happyfood/activity/MainActivity.kt
+++ app3/src/main/java/com/intern/happyfood/activity/MainActivity.kt
@@ -4,9 +4,23 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.FrameLayout
+import androidx.appcompat.widget.Toolbar
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.intern.happyfood.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var coordinatorLayout: CoordinatorLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var frameLayout: FrameLayout
+    private lateinit var navigationView: NavigationView
+
+    private var previousMenuItem: MenuItem? = null
 
     /*Life-cycle method*/
     override fun onCreate(savedInstanceState: Bundle?) {
